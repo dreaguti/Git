4b424c66e86cae52477f678139ebdb3f03ce6178
Andrea <drea.guti@gmail.com>
2020-04-06 18:33:01 -0400

    update Login.java

--- src/Test/Login.java
+++ src/Test/Login.java
@@ -3,13 +3,10 @@ package Test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("Hello!");
-<<<<<<< HEAD
         System.out.println("Logging in..");
 
         //this line is written from IntelliJ
-=======
         
-        //github to intellij 
->>>>>>> 83c898892403d19d13e70ed12ca7be5092b04a20
+        //github to intellij
     }
 }
